754e16892d9f96dfa05ccf39058f5387df82d15f
trananhtuuuuuuuu <trananhtu01012004@gmail.com>
2025-11-07 12:15:33 +0700

    set up project

---
+++ music-api-app/src/main/java/org/example/musicapiapp/MusicApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.musicapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApiAppApplication.class, args);
+    }
+
+}
---
+++ music-api-app/src/test/java/org/example/musicapiapp/MusicApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musicapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
